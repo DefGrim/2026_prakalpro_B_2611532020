13a36916e642cce4d22ce3c62a18456ef2a4d510
DefGrim <defrannaufalanski2@gmail.com>
2026-09-10 09:05:01 +0700

    pekan2

---
+++ pekan2/numerik_2611532020.py
@@ -0,0 +1,13 @@
+# Buat file dengan nama Numerik_Nim.py
+# nama variabel ditambah 4 digit terakhir NIM contoh: a_5500
+#create a variable with numerix type value.
+a=100
+print("The type of variable having value", a, " is ", type(a_2020))
+
+#create a variable with float value
+b=10.2345
+print("The type of variable having value", b, " is ", type(b_2020))
+
+#create a variable with complex value.
+c=100+3j
+print("The type of variable having value", c, " is ", type(c_2020))
---
+++ pekan2/string_2611532020.py
@@ -0,0 +1,15 @@
+# Buat file dengan nama String_NIM.py
+#nama variabel ditambah 4 digit nim terakhir contoh: a_5500
+#ganti programnya dan data masing2
+nama_2020 = "Defran"
+jenis_kelamin ='L'
+alamat = """
+   Kampus Unand
+   kec.pauh
+   kota Padang
+"""
+agama = "Islam"
+print("Nama saya adalah:", nama)
+print("Jenis kelamin saya adalah:", jenis_kelamin)
+print("Alamat saya adalah:", alamat)
+print("Agama saya adalah:", agama)
