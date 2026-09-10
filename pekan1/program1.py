2d2cbfbb8eb997c00126081684ace38cd20f6500
DefGrim <defrannaufalanski2@gmail.com>
2026-09-10 14:36:48 +0700

    Fixed some changes on laprak1

--- pekan1/program1.py
+++ pekan1/program1.py
@@ -2,12 +2,12 @@
 """
 Komentar
 dua baris atau lebih
-pertemuan 1 pratikum alpro
+pertemuan 1 praktikum alpro
 """
 print("Hello World")
 print()
 print("program ini menghasilkan")
 print("empat baris output")
 print("/ \\\\ / \\\\ / \\\\ / \\\\ / ")
-print("program ini" +" dibuat oleh \n"
+print("program ini" +" dibuat oleh b\n"
 + "Defran Naufalanski 2611532020" )
