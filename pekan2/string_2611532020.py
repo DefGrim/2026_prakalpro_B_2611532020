559b22a65f80322e093ba846e3f925cfe373c89e
DefGrim <defrannaufalanski2@gmail.com>
2026-09-10 09:07:31 +0700

    pekan2

--- pekan2/string_2611532020.py
+++ pekan2/string_2611532020.py
@@ -2,14 +2,14 @@
 #nama variabel ditambah 4 digit nim terakhir contoh: a_5500
 #ganti programnya dan data masing2
 nama_2020 = "Defran"
-jenis_kelamin ='L'
-alamat = """
-   Kampus Unand
+jenis_kelamin_2020 ='L'
+alamat_2020 = """
+   Kos Airi
    kec.pauh
    kota Padang
 """
 agama = "Islam"
-print("Nama saya adalah:", nama)
-print("Jenis kelamin saya adalah:", jenis_kelamin)
-print("Alamat saya adalah:", alamat)
-print("Agama saya adalah:", agama)
+print("Nama saya adalah:", nama_2020)
+print("Jenis kelamin saya adalah:", jenis_kelamin_2020)
+print("Alamat saya adalah:", alamat_2020)
+print("Agama saya adalah:", agama_2020)
