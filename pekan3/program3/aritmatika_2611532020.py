a404bbb19e5e2e381caf4cf7571bcc7151fd1fbf
DefGrim <defrannaufalanski2@gmail.com>
2026-09-17 08:28:15 +0700

    added pekan3

---
+++ pekan3/program3/aritmatika_2611532020.py
@@ -0,0 +1,18 @@
+# Buat file dengan nama aritmatika_NIM.py
+# Buat program untuk operator aritmatika dalam Python
+# Nama variabel ditambah ] 4 digit nim terakhir contoh: angka1_1234
+# Program ini menggunakan fungsi input()
+# Nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+
+angka1_2020 = int(input("Input angka-1: "))
+angka2_2020 = int(input("Input angka-2: "))
+
+# Penjumlahan
+hasil = angka1_2020 + angka2_2020
+print("\nOperator Penjumlahan")
+print("Hasil:", hasil)
+
+# Pengurangan
+hasil = angka1_2020 - angka2_2020   
+print("\nOperator Pengurangan")
+print("Hasil:", hasil)
